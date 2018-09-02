b670d63a2e39a43494159dec6b9c8e68b0eafccb
Piotr Kwasnik <kwasnik.piotr@gmail.com>
2018-09-02 22:08:24 +0200

    adds decorator with parameters

--- code/12/sandbox.py
+++ code/12/sandbox.py
@@ -30,6 +30,7 @@ def configure_arg_parser():
     return arg_parser
 
 
+# Decorator
 def logged(func):
     """
     adds logging to passed function
@@ -38,7 +39,7 @@ def logged(func):
     :return: wrapped function
     """
 
-    logging.debug('Adding logging to {}'.format(func.__name__))
+    # logging.debug('Adding logging to {}'.format(func.__name__))
 
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -48,8 +49,54 @@ def logged(func):
     return wrapper
 
 
+# Decorator with parameters
+def logged_formatted(fmt):
+    """"
+    ads formatter to logging function
+    """
+    def logged(func):
+        """
+        adds logging to passed function
+
+        :param funct:
+        :return: wrapped function
+        """
+
+        # logging.debug('Adding logging to {}'.format(func.__name__))
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(fmt.format(func=func))
+            return func(*args, **kwargs)
+
+        return wrapper
+    return logged
+
+
 @logged
-def add(x, y):
+def add1(x, y):
+    logging.debug('adding: {} + {} -> {}'.format(x, y, x + y))
+    return x + y
+
+
+@logged_formatted('Executing: {func.__name__}()')
+def add2(x, y):
+    logging.debug('adding: {} + {} -> {}'.format(x, y, x + y))
+    return x + y
+
+
+# changing logged to use logged formated as default
+logged = logged_formatted('Function call: {func.__name__}()')
+
+
+@logged
+def add3(x, y):
+    logging.debug('adding: {} + {} -> {}'.format(x, y, x + y))
+    return x + y
+
+
+@logged
+def add4(x, y):
     logging.debug('adding: {} + {} -> {}'.format(x, y, x + y))
     return x + y
 
@@ -64,6 +111,11 @@ if __name__ == '__main__':
     else:
         logging.getLogger().setLevel(logging.INFO)
 
-
-    add(5, 6)
+    # Checking logged decorator
+    add1(5, 6)
+    # Checking logged_formatter decorator
+    add2(5, 6)
+    # Checking logged set as logged_formatter decorator
+    add3(5, 6)
+    add4(5, 6)
 
