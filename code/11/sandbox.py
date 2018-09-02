b300a2cef499fe50513b3c913ef4f51ee79423e3
Piotr Kwasnik <kwasnik.piotr@gmail.com>
2018-09-02 15:48:18 +0200

    adds closure functions to perform type checking

--- code/11/sandbox.py
+++ code/11/sandbox.py
@@ -46,6 +46,54 @@ def add(x, y):
     return do_add
 
 
+# using closure functions to perform type checking
+def typed_property(name, expected_type):
+    private_name = '_' + name
+
+    @property
+    def property_generator(self):
+        logging.debug('Running getter property generator for: {}'.format(name))
+        return getattr(self, private_name)
+
+    @property_generator.setter
+    def property_generator(self, value):
+        logging.debug('Running setter property generator for: {}'.format(name))
+        if isinstance(value, expected_type):
+            setattr(self, private_name, value)
+        else:
+            raise TypeError('Expected type for {}: {}'.format(name, expected_type))
+
+    return property_generator
+
+
+class Point(object):
+
+    position_x = typed_property('position_x ', int)
+    position_y = typed_property('position_y ', int)
+
+    def __init__(self, x, y):
+        self.position_x = x
+        self.position_y = y
+
+
+Integer = lambda name: typed_property(name, int)
+Float = lambda name: typed_property(name, float)
+String = lambda name: typed_property(name, str)
+
+
+class Point2(object):
+
+    position_x = Integer('position_x')
+    position_y = Integer('position_y')
+
+    # print(repr(position_y))
+    # print(type(position_y))
+
+    def __init__(self, x, y):
+        self.position_x = x
+        self.position_y = y
+
+
 if __name__ == '__main__':
 
     parser = configure_arg_parser()
@@ -53,6 +101,16 @@ if __name__ == '__main__':
 
     if arguments.debug:
         logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
 
-    delay(3, hello)
-    delay(3, add(3, 4))
+    # checking basic usage of closure functions
+    # delay(3, hello)
+    # delay(3, add(3, 4))
+
+    # checking type checking with closure functions
+    # first_point = Point(1, 4)
+    # first_point = Point(1, '4')
+    second_point = Point2(1, 4)
+    # second_point = Point2(1, '4')
+
